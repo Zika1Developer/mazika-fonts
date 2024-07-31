a009031123054ad670c8b49ad7332e88fcfa96f5
MaZika1Dev <132018840+Zika1Developer@users.noreply.github.com>
2024-07-31 17:34:23 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,19 @@
+const bold = require("./bold.js");
+const MTBold = require("./Script-MT-Bold.js")
+const Edwardian = require("./Edwardian.js")
+const Fraktur = require("./Fraktur.js")
+const buckle = require("./buckle.js")
+const Italic = require("./Italic.js")
+const serif = require("./sans-serif.js")
+const mazika = require('./Sans.js')
+
+module.exports = {
+  bold, 
+  MTBold, 
+  Edwardian, 
+  Fraktur, 
+  buckle, 
+  Italic, 
+  serif,
+  mazika
+}
